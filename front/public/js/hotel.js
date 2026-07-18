34ffa3358188681aefe47a01f00b8787a4187552
marioaje <marioaje@gmail.com>
2026-07-17 19:10:49 -0600

    hotel cargar datos individual

--- front/public/js/hotel.js
+++ front/public/js/hotel.js
@@ -27,6 +27,9 @@ function pintarTabla(dataLista) {
 
         let fila = `
           <tr class="table-primary">
+                  <td>             
+                     <a name="" id="" class="btn btn-warning" role="button" onclick="cargarDatosFormulario(${elementoIndividual.id})">Editar</a
+                ></td>
                   <td scope="row">${elementoIndividual.id}</td>
                   <td>${elementoIndividual.nombre}</td>
                   <td>${elementoIndividual.descripcion}</td>
@@ -43,18 +46,40 @@ function pintarTabla(dataLista) {
 
     });
 
-    // npm install
-    // id = 0,
-    // nombre = "",
-    // descripcion = "",
-    // telefono = "",
-    // correo = "",
-    // sitio_web = "",
-    // usuario = "",
-    // fecha_creacion = "",
-    // estado = ""
-
-    console.info(data);
-    console.log("Dibujando tabla");
 }
 
+window.cargarDatosFormulario = function (id) {
+    alert("cargando datos en el formulario" + id);
+
+    //Cargar los datos del hotel en el formulario
+    //GET https://paginas-web-cr.com/Api/hotelApi/hotel/hotel.php?id=1 
+    fetch(`${API.HOTEL}?id=${id}`, { method: 'GET' })
+        .then(response => response.json())
+        .then(data => formularioPintar(data.data[0]))
+        .catch(error => console.error(error));
+
+}
+
+function formularioPintar(data) {
+
+    document.getElementById('id').value = data.id;
+    document.getElementById('nombre').value = data.nombre;
+    document.getElementById('descripcion').value = data.descripcion;
+    document.getElementById('telefono').value = data.telefono;
+    document.getElementById('correo').value = data.correo;
+    document.getElementById('sitio_web').value = data.sitio_web;
+    // document.getElementById('usuario').value = data.usuario;
+    // document.getElementById('fecha_creacion').value = data.fecha_creacion;
+    document.getElementById('estado').value = data.estado;
+
+    const modalHotel = new bootstrap.Modal(document.getElementById('modalHotel'));
+    modalHotel.show();
+}
+
+
+// function cargarDatosFormulario(id) {
+//     alert("cargando datos en el formulario" + id);
+
+//     //Cargar los datos del hotel en el formulario
+
+// }
---
+++ front/public/js/menu.js
@@ -0,0 +1,9 @@
+let menu = `
+     <nav class="nav justify-content-center">
+        <a class="nav-link" href="index.html">Inicio</a>
+        <a class="nav-link" href="hotel.html">Hoteles</a>
+        <a class="nav-link disabled" href="#">Disabled link</a>
+      </nav>
+        `;
+
+document.getElementById("baseMenu").innerHTML = menu;
